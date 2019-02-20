38b3f200cfe6e4a6e91d06e09a3bf62577fb8db7
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-02-20 12:46:38 -0500

    Add constructor parameters to GloApi

--- src/GloApi.kt
+++ src/GloApi.kt
@@ -1,3 +1,21 @@
 package net.publicmethod
 
-class GloApi
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
+import io.ktor.util.KtorExperimentalAPI
+
+class GloApi @KtorExperimentalAPI constructor(
+    val personalAuthenticationToken: String,
+    val httpClient: HttpClient = HttpClient(CIO) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+    }
+)
