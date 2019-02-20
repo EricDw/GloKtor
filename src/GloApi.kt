89aea8c70d793bedd14e9ca15e6c1691733a1534
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-02-20 13:55:19 -0500

    Add and test getUser function

--- src/GloApi.kt
+++ src/GloApi.kt
@@ -6,16 +6,35 @@ import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.client.request.url
 import io.ktor.util.KtorExperimentalAPI
+import net.publicmethod.dtos.GloUserDTO
 
 class GloApi @KtorExperimentalAPI constructor(
-    val personalAuthenticationToken: String,
-    val httpClient: HttpClient = HttpClient(CIO) {
+    private val personalAuthenticationToken: String,
+    private val logLevel: LogLevel = LogLevel.NONE,
+    private val httpClient: HttpClient = HttpClient(CIO) {
         install(JsonFeature) {
             serializer = GsonSerializer()
         }
         install(Logging) {
-            level = LogLevel.HEADERS
+            level = logLevel
         }
     }
 )
+{
+    suspend fun getUser(): GloUserDTO =
+        httpClient.get {
+            url(buildGetUserURL())
+        }
+
+
+    private fun buildGetUserURL(): String =
+        "$BASE_URL$USER_ENDPOINT$QUERY_ACCESS_TOKEN$personalAuthenticationToken"
+
+}
+
+private const val BASE_URL = "https://gloapi.gitkraken.com/v1/glo/"
+private const val USER_ENDPOINT = "user"
+private const val QUERY_ACCESS_TOKEN = "?access_token="
--- test/GloApiTests.kt
+++ test/GloApiTests.kt
@@ -7,10 +7,12 @@ import io.ktor.client.engine.mock.MockHttpResponse
 import io.ktor.client.engine.mock.responseError
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.request.get
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
+import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.io.ByteReadChannel
 import kotlinx.coroutines.runBlocking
 import net.publicmethod.dtos.GloUserDTO
@@ -59,5 +61,49 @@ class GloApiTests
             assertEquals("Not Found other/path", client.get("/other/path"))
         }
     }
+
+    @KtorExperimentalAPI
+    @Test
+    fun `given PAT when getUser then return Deferred of GloUserDTO`() = runBlocking {
+
+        // Arrange
+        val userJson = """{"id":"some-gi-ber-ish","username":"Test User"}"""
+        val client = HttpClient(MockEngine {
+            when (url.encodedPath)
+            {
+                "/v1/glo/user" ->
+                {
+                    MockHttpResponse(
+                        call,
+                        HttpStatusCode.OK,
+                        ByteReadChannel(userJson.toByteArray(Charsets.UTF_8)),
+                        headersOf("Content-Type", ContentType.Application.Json.toString())
+                    )
+                }
+                else ->
+                    responseError(HttpStatusCode.NotFound, "Not Found ${url.encodedPath}")
+            }
+        }) {
+
+            install(JsonFeature) {
+                serializer = GsonSerializer()
+            }
+            expectSuccess = false
+        }
+
+        val gloApi = GloApi(
+            personalAuthenticationToken = "test-pat",
+            logLevel = LogLevel.ALL,
+            httpClient = client
+        )
+        val expected = GloUserDTO(id = "some-gi-ber-ish", userName = "Test User")
+
+        // Act
+        val actual = gloApi.getUser()
+
+        // Assert
+        assertEquals(expected, actual)
+    }
 }
+
 
