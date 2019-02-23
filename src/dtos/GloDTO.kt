34c2179ab9222777017be7e3fc0c218ba57a8582
EricD <eric.dewildt@compassdigital.io>
2019-02-22 21:17:43 -0500

    Add internal to all the DTOs

--- src/dtos/GloDTO.kt
+++ src/dtos/GloDTO.kt
@@ -1,10 +1,10 @@
 package net.publicmethod.dtos
 
-sealed class GloDTO
+internal sealed class GloDTO
 
-typealias BoardDTOs = List<BoardDTO>
+internal typealias BoardDTOs = List<BoardDTO>
 
-data class BoardDTO(
+internal data class BoardDTO(
     val id: String,
     val name: String,
     val archived_columns: ArchivedColumnDTOs?,
@@ -17,35 +17,35 @@ data class BoardDTO(
     val members: MemberDTOs?
 ) : GloDTO()
 
-typealias ArchivedColumnDTOs = List<ArchivedColumnDTO>
+internal typealias ArchivedColumnDTOs = List<ArchivedColumnDTO>
 
-data class ArchivedColumnDTO(
+internal data class ArchivedColumnDTO(
     val id: String,
     val name: String,
     val archived_date: String?,
     val created_date: String?
 ) : GloDTO()
 
-typealias ColumnDTOs = List<ColumnDTO>
+internal typealias ColumnDTOs = List<ColumnDTO>
 
-data class ColumnDTO(
+internal data class ColumnDTO(
     val id: String,
     val name: String,
     val archived_date: String?,
     val created_date: String?
 ) : GloDTO()
 
-typealias InvitedMemberDTOs = List<InvitedMemberDTO>
+internal typealias InvitedMemberDTOs = List<InvitedMemberDTO>
 
-data class InvitedMemberDTO(
+internal data class InvitedMemberDTO(
     val id: String,
     val username: String,
     val role: String?
 ) : GloDTO()
 
-typealias LabelDTOs = List<LabelDTO>
+internal typealias LabelDTOs = List<LabelDTO>
 
-data class LabelDTO(
+internal data class LabelDTO(
     val id: String,
     val name: String,
     val color: ColorDTO?,
@@ -53,26 +53,26 @@ data class LabelDTO(
     val created_date: String?
 ) : GloDTO()
 
-typealias MemberDTOs = List<MemberDTO>
+internal typealias MemberDTOs = List<MemberDTO>
 
-data class MemberDTO(
+internal data class MemberDTO(
     val id: String,
     val username: String,
     val role: String?
 ) : GloDTO()
 
-data class CreatedByDTO(
+internal data class CreatedByDTO(
     val id: String?
 ) : GloDTO()
 
-data class ColorDTO(
+internal data class ColorDTO(
     val r: Int?,
     val g: Int?,
     val b: Int?,
     val a: Int?
 ) : GloDTO()
 
-data class GloUserDTO(
+internal data class GloUserDTO(
     val id: String,
     val username: String
 ) : GloDTO()
