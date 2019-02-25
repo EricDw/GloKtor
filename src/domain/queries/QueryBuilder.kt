57fbcb1ed063ac19cd64fd353be3bf2f35c257f3
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-02-25 18:01:46 -0500

    Add abtract QueryBuilder class

---
+++ src/domain/queries/QueryBuilder.kt
@@ -0,0 +1,29 @@
+package domain.queries
+
+abstract class QueryBuilder<T : QueryBuilder.QueryParameter>
+{
+    private val _parameters: MutableMap<String, MutableSet<String>> = mutableMapOf()
+
+    fun addParameter(userQueryParameter: T): Unit =
+        userQueryParameter.run {
+            if (_parameters.containsKey(key))
+            {
+                _parameters[key]?.add(userQueryParameter.value)
+            } else
+            {
+                _parameters[key] = mutableSetOf(userQueryParameter.value)
+            }
+        }
+
+    fun build(): Query = Query(_parameters)
+
+    abstract class QueryParameter
+    {
+        abstract val key: String
+        abstract val value: String
+    }
+}
+
+typealias QueryParameters = Map<String, Set<String>>
+
+data class Query(val userQueryParameters: QueryParameters = mapOf())
