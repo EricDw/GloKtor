9a2e33e409e504a5736b08db01ee9b0b45c0b8c2
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-03-23 15:09:19 -0400

    Refactor test Constants

--- test/glo_api/SharedTestConstants.kt
+++ test/glo_api/SharedTestConstants.kt
@@ -1,50 +1,34 @@
 package glo_api
 
-
+const val TEST_ID_1 = "some-gi-ber-ish1"
+const val TEST_ID_2 = "some-gi-ber-ish2"
 const val TEST_PAT = "test-pat"
-
 const val TEST_NAME = "test name"
 
 const val TEST_BOARD_NAME_1 = "Test Board1"
-
 const val TEST_BOARD_NAME_2 = "Test Board2"
+const val TEST_BOARD_ID_1 = TEST_ID_1
+const val TEST_BOARD_ID_2 = TEST_ID_2
 
-const val TEST_USER_ID_1 = "some-gi-ber-ish"
+const val TEST_USER_ID_1 = TEST_ID_1
+const val TEST_USER_ID_2 = TEST_ID_2
 
-const val TEST_USER_ID_2 = "some-gi-ber-ish"
-
-const val TEST_BOARD_ID_1 = "some-gi-ber-ish1"
-
-const val TEST_BOARD_ID_2 = "some-gi-ber-ish2"
-
-const val TEST_COLUMN_ID_1 = "some-gi-ber-ish1"
-
-const val TEST_COLUMN_ID_2 = "some-gi-ber-ish2"
-
+const val TEST_COLUMN_ID_1 = TEST_ID_1
+const val TEST_COLUMN_ID_2 = TEST_ID_2
 const val TEST_COLUMN_NAME_1 = "Test Column1"
-
 const val TEST_COLUMN_NAME_2 = "Test Column2"
 
-const val TEST_CARD_ID_1 = "some-gi-ber-ish1"
-
-const val TEST_CARD_ID_2 = "some-gi-ber-ish2"
-
+const val TEST_CARD_ID_1 = TEST_ID_1
+const val TEST_CARD_ID_2 = TEST_ID_2
 const val TEST_CARD_NAME_1 = "Test Card1"
-
 const val TEST_CARD_NAME_2 = "Test Card2"
 
-const val TEST_ATTACHMENT_ID_1 = "some-gi-ber-ish1"
-
-const val TEST_ATTACHMENT_ID_2 = "some-gi-ber-ish2"
-
+const val TEST_ATTACHMENT_ID_1 = TEST_ID_1
+const val TEST_ATTACHMENT_ID_2 = TEST_ID_2
 const val TEST_FILE_NAME_1 = "Test Card1"
-
 const val TEST_FILE_NAME_2 = "Test Card2"
 
-const val TEST_COMMENT_ID_1 = "some-gi-ber-ish1"
-
-const val TEST_COMMENT_ID_2 = "some-gi-ber-ish2"
-
+const val TEST_COMMENT_ID_1 = TEST_ID_1
+const val TEST_COMMENT_ID_2 = TEST_ID_2
 const val TEST_TEXT_1 = "Some random text1"
-
 const val TEST_TEXT_2 = "Some random text2"
