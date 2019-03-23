aa5a4c95a996dbee6418b9d6f7661a84ca48d6ae
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-03-23 15:10:45 -0400

    Move two lines down

--- test/glo_api/SharedTestConstants.kt
+++ test/glo_api/SharedTestConstants.kt
@@ -5,10 +5,10 @@ const val TEST_ID_2 = "some-gi-ber-ish2"
 const val TEST_PAT = "test-pat"
 const val TEST_NAME = "test name"
 
-const val TEST_BOARD_NAME_1 = "Test Board1"
-const val TEST_BOARD_NAME_2 = "Test Board2"
 const val TEST_BOARD_ID_1 = TEST_ID_1
 const val TEST_BOARD_ID_2 = TEST_ID_2
+const val TEST_BOARD_NAME_1 = "Test Board1"
+const val TEST_BOARD_NAME_2 = "Test Board2"
 
 const val TEST_USER_ID_1 = TEST_ID_1
 const val TEST_USER_ID_2 = TEST_ID_2
